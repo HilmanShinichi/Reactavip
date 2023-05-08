f30f34989f226bcd861849c86b4252f9f666f052
HilmanShinichi <hilmanseptiana19@gmail.com>
2023-05-08 16:35:58 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Button = (props) => {
+  const {children, variant = "bg-black"} = props;
+  return (
+    <button
+      className={`h-10 px-6 font-semibold rounded-md ${variant} text-white`}
+      type="submit"
+    >
+      {children}
+    </button>
+  );
+};
+
+function App() {
+  return (
+    <div className="flex justify-center bg-blue-600 min-h-screen items-center">
+      <div className="flex gap-x-3">
+        <Button variant="bg-[red]">Login</Button>
+        <Button variant="bg-[black]">Daftar</Button>
+        <Button variant="bg-[yellow]">Coba</Button>
+        <Button></Button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
