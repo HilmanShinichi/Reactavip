631324d9e02bbb3b9e7e10fde4befb5df8c1560d
Hilman Shinichi <hilmanseptiana19@gmail.com.com>
2023-06-18 21:01:52 +0700

    list rendering

--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -1,30 +1,48 @@
 
 import CardProduct from '../components/Fragments/CardProduct';
 
+const products = [
+  {
+    id: 1,
+    image: "/public/images/shoes1.jpg",
+    name: "Sepatu Baru",
+    price: "Rp 900.000",
+    desc:` Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod
+    iusto dolor repellendus aliquid temporibus labore laudantium eum
+    deserunt, voluptas eos quisquam, quaerat consequatur eaque
+    voluptate nemo minus odio, provident quas.`
+  },
+  {
+    id: 2,
+    image: "/public/images/shoes1.jpg",
+    name: "Sepatu Lama",
+    price: "Rp 500.000",
+    desc:` Lorem ipsum dolor sit amet consectetur adipisicing elit.`
+  },
+  {
+    id: 3,
+    image: "/public/images/shoes1.jpg",
+    name: "Sepatu Hits",
+    price: "Rp 700.000",
+    desc:`salah satu sepatu dari brand terkemuka adadong`
+  }
+]
+
 
 const ProductPage = () => {
   return (
     <div className="flex justify-center py-5 gap-5">
-      <CardProduct>
-        <CardProduct.header image="/public/images/shoes1.jpg"></CardProduct.header>
-        <CardProduct.body title="Sepatu Baru">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod
-            iusto dolor repellendus aliquid temporibus labore laudantium eum
-            deserunt, voluptas eos quisquam, quaerat consequatur eaque
-            voluptate nemo minus odio, provident quas.
-        </CardProduct.body>
-        <CardProduct.footer price="Rp 900.000"></CardProduct.footer> 
-      </CardProduct>
-      <CardProduct>
-        <CardProduct.header image="/public/images/shoes1.jpg"></CardProduct.header>
-        <CardProduct.body title="Sepatu Baru">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod
-            iusto dolor repellendus aliquid temporibus labore laudantium eum
-            deserunt, voluptas eos quisquam, quaerat consequatur eaque
-            voluptate nemo minus odio, provident quas.
-        </CardProduct.body>
-        <CardProduct.footer price="Rp 900.000"></CardProduct.footer> 
-      </CardProduct>
+      {
+        products.map((product) => (
+          <CardProduct key={product.id}>
+          <CardProduct.header image={product.image}></CardProduct.header>
+          <CardProduct.body name={product.name}>
+            {product.desc}
+          </CardProduct.body>
+          <CardProduct.footer price={product.price}></CardProduct.footer> 
+        </CardProduct>
+        ))
+      }   
     </div>
   );
 };
